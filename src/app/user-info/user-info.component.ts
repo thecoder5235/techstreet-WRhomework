7fb57614cd37394ca352488cf5e51928684671c0
thecoder5235 <brosati35@hotmail.com>
2017-11-09 15:29:11 -0500

    Update user-info.component.ts

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,1 +1,24 @@
 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+
+  selector: 'user-info-component',
+
+  templateUrl: './user-info.component.html',
+
+  styleUrls: ['./user-info.component.css']
+
+})
+
+export class UserInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+
+
+}
