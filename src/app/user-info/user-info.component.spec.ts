5a3b49ce39d52f1f1eba391c2d342aa15301121b
thecoder5235 <brosati35@hotmail.com>
2017-11-09 15:28:06 -0500

    Update user-info.component.spec.ts

--- src/app/user-info/user-info.component.spec.ts
+++ src/app/user-info/user-info.component.spec.ts
@@ -1,1 +1,51 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule, MdCardModule, MdGridListModule, MdIconModule, MdToolbarModule, MdButtonModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { LogoutComponent } from '../account/logout.component';
+import { UserDetailComponent } from '../user/detail.component';
+import { APP_BASE_HREF } from '@angular/common';
 
+import { UserInfoComponent } from './user-info.component';
+
+describe('UserInfoComponent', () => {
+
+  let component: UserInfoComponent;
+
+  let fixture: ComponentFixture<UserInfoComponent>;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+
+      providers: [ {provide: APP_BASE_HREF, useValue: '/'} ],
+
+      declarations: [ UserInfoComponent, DashboardComponent, LogoutComponent, UserDetailComponent ],
+
+      imports: [ MaterialModule.forRoot(), RouterModule, AppRoutingModule ]
+
+    })
+
+    .compileComponents();
+
+  }));
+
+  beforeEach(() => {
+
+    fixture = TestBed.createComponent(UserInfoComponent);
+
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+  });
+
+
+  it('should create', () => {
+
+    expect(component).toBeTruthy();
+
+  });
+
+});
