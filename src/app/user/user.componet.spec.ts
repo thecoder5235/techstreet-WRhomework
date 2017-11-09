ad9ead4066daf527e4e6f6e359f28ef77cdbb6d5
thecoder5235 <brosati35@hotmail.com>
2017-11-09 14:12:41 -0500

    Update user.componet.spec.ts

--- src/app/user/user.componet.spec.ts
+++ src/app/user/user.componet.spec.ts
@@ -1,1 +1,60 @@
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { MaterialModule, MdCardModule, MdGridListModule, MdIconModule, MdToolbarModule, MdButtonModule } from '@angular/material';
+
+import { UserDetailComponent } from './detail.component';
+
+import { AppRoutingModule } from '../app-routing.module';
+
+import { APP_BASE_HREF } from '@angular/common';
+
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+import { LogoutComponent } from '../account/logout.component';
+
+
+
+
+
+describe('UserDetailComponent', () => {
+
+  let component: UserDetailComponent;
+
+  let fixture: ComponentFixture<UserDetailComponent>;
+
+
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+
+      declarations: [ UserDetailComponent, DashboardComponent, LogoutComponent ],
+
+      imports: [ MaterialModule.forRoot(), RouterModule, AppRoutingModule ],
+
+      providers: [ {provide: APP_BASE_HREF, useValue: '/'} ]
+
+    })
+
+    .compileComponents();
+
+  }));
+
+
+
+  beforeEach(() => {
+
+    fixture = TestBed.createComponent(UserDetailComponent);
+
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+  });
+
+
+
+});
