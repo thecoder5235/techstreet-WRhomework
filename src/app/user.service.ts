daee2daf84b8ffc9e7f2b0d6dc95c7d51277f5d7
thecoder5235 <brosati35@hotmail.com>
2017-11-09 16:01:34 -0500

    Update user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,60 +1,50 @@
+
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/map';
+import { User } from './user';
 
 @Injectable()
+
 export class UserService {
-  users: any[] = [
-    { first_name: "Aesop",     last_name: "Hildebrand" },
-    { first_name: "Aladdin",   last_name: "Tirzah" },
-    { first_name: "Launce",    last_name: "Aspasia" },
-    { first_name: "Sumati",    last_name: "Parthalan" },
-    { first_name: "Cassia",    last_name: "Amalbert" },
-    { first_name: "Mitra",     last_name: "Gemariah" },
-    { first_name: "Josiah",    last_name: "Gershon" },
-    { first_name: "Aelia",     last_name: "Ishmael" },
-    { first_name: "Neasa",     last_name: "Yahveh" },
-    { first_name: "Matthia",   last_name: "Jada" },
-    { first_name: "Israel",    last_name: "Haidee" },
-    { first_name: "Samuel",    last_name: "Rouben" },
-    { first_name: "Laila",     last_name: "Jubal" },
-    { first_name: "Reed",      last_name: "Herminio" },
-    { first_name: "Eli",       last_name: "Errol" },
-    { first_name: "Doretta",   last_name: "Trinity" },
-    { first_name: "Dorothea",  last_name: "Trenton" },
-    { first_name: "Scot",      last_name: "Catalina" },
-    { first_name: "Sheena",    last_name: "Kiley" },
-    { first_name: "Maybelle",  last_name: "Yishai" },
-    { first_name: "Melisa",    last_name: "Kilian" },
-    { first_name: "Ethelyn",   last_name: "Lora" },
-    { first_name: "Carey",     last_name: "Elaine" },
-    { first_name: "Danica",    last_name: "Niven" },
-    { first_name: "Geordie",   last_name: "Flannan" },
-    { first_name: "Esmeralda", last_name: "Lexy" },
-    { first_name: "Vince",     last_name: "Jemmy" },
-    { first_name: "Lincoln",   last_name: "Briseida" },
-    { first_name: "Judy",      last_name: "Blaine" },
-    { first_name: "Gerry",     last_name: "Darcy" }
-  ]
 
-  roles: string[] = [
-    "admin",
-    "editor",
-    "purchaser",
-    "service",
-    "standard"
-  ]
 
-  constructor() {}
 
-  index() {
-    return this.users.map(user => {
-      const email = user.first_name.toLowerCase().substring(0,3)
-                  + user.last_name.toLowerCase().substring(0,4)
-                  + "@test.com";
-      const name_length = user.first_name.length + user.last_name.length;
-      const role_index = name_length % this.roles.length;
-      const role = this.roles[role_index];
-      return Object.assign(user, { email: email, role: role });
-    });
+  users: User[] = [];
+
+
+
+  public serverResponse = '{ "email": [ "can\'t be blank" ], "first_name": [ "can\'t be blank" ], "last_name": [ "can\'t be blank" ] }'; 
+
+  private userDictionary = { 
+
+    1: '{ "id": 1, "buyer_id": 1, "first_name": "josh", "last_name": "Hoskins", "email": "josh.hoskins@sgmail.com" }',
+
+    2: '{ "id": 2, "buyer_id": 2, "first_name": "lance", "last_name": "salkind", "email": "lance.salkind@sgmail.com" }',
+
+    3: '{ "id": 3, "buyer_id": 3, "first_name": "Will", "last_name": "Rosati", "email": "will.Rosati@gmail.com" }'
+
+  };
+
+
+  constructor() { }
+
+  getUsers() {
+
+   this.users = JSON.parse("["+Object.keys(this.userDictionary).map(key => this.userDictionary[key]).join(',')+"]");
+
   }
+
+  getUser(id: number) {
+
+    return JSON.parse(this.userDictionary[id]);
+
+  }
+
+  private logError(error: any) {
+
+    console.error('service found an error: '+error);
+
+  }
+
+
+
 }
