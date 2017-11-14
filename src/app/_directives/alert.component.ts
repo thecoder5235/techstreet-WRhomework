ae6abd3809f35ab0cf00e0137bd3ce11793afff2
thecoder5235 <brosati35@hotmail.com>
2017-11-14 14:04:48 -0500

    Update alert.component.ts

--- src/app/_directives/alert.component.ts
+++ src/app/_directives/alert.component.ts
@@ -1,1 +1,27 @@
 
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../_services/index';
+
+@Component({
+
+    moduleId: module.id.toString(),
+
+    selector: 'alert',
+
+    templateUrl: 'alert.component.html'
+
+})
+
+export class AlertComponent {
+
+    message: any;
+
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit() {
+
+        this.alertService.getMessage().subscribe(message => { this.message = message; });
+
+    }
+
+}
